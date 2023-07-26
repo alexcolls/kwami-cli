7780347e0b73bc63836828894d9fbeb61d24d96f
quantium-rock <alexcollsoutumuro@gmail.com>
2023-07-26 15:38:21 +0200

    init kwami v.0.0.0

--- clients/kapps/kwami/vanilla/tailwind.config.js
+++ clients/kapps/kwami/vanilla/tailwind.config.js
@@ -1,4 +1,4 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import('./vanilla/node_modules/tailwindcss/types').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
