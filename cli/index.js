1f74e251e4d77cc439c37b82c4fb91b64a3ceea7
quantium-rock <alexcollsoutumuro@gmail.com>
2023-07-27 14:46:31 +0200

    add kwami-cli v.0.0.0

--- cli/index.js
+++ cli/index.js
@@ -1,1 +1,102 @@
-import "./package";
+#!/usr/bin/env node
+
+const { program } = require("commander");
+const figlet = require("figlet");
+const shell = require("shelljs");
+const inquirer = require("inquirer");
+
+// Function to display ASCII art
+function displayAsciiArt() {
+  figlet("Kwami CLI", (err, data) => {
+    if (err) {
+      console.error("Error displaying ASCII art:", err);
+      return;
+    }
+    console.log(data);
+  });
+}
+
+// Function to display main options
+function displayMainOptions() {
+  console.log("\n> options:");
+  console.log("   1. create");
+  console.log("   2. test");
+  console.log("   3. build");
+  console.log("   4. deploy");
+  console.log("   5. ? help");
+}
+
+// Function to display create options
+async function displayCreateOptions() {
+  console.log("\n> options:");
+  console.log("   1. kapp");
+  console.log("   2. kwam");
+  console.log("   3. vite");
+  console.log("   4. ? help");
+  console.log("   5. < back");
+
+  const answer = await inquirer.prompt([
+    {
+      type: "input",
+      name: "option",
+      message: "Choose an option:",
+    },
+  ]);
+
+  switch (answer.option.toLowerCase()) {
+    case "1":
+    case "kapp":
+      // Implement logic for copying default kapp folder
+      console.log("Creating a new kapp project...");
+      break;
+    case "2":
+    case "kwam":
+      // Implement logic for copying default kwam folder
+      console.log("Creating a new kwam project...");
+      break;
+    case "3":
+    case "vite":
+      // Implement logic for creating a new vite project
+      console.log("Creating a new vite project...");
+      break;
+    case "4":
+    case "?":
+    case "help":
+      displayHelp();
+      break;
+    case "5":
+    case "<":
+    case "back":
+      displayMainOptions();
+      break;
+    default:
+      console.log("Invalid option. Please select again.");
+      displayCreateOptions();
+      break;
+  }
+}
+
+// Function to display help message
+function displayHelp() {
+  // Implement help content here
+  console.log("Help content goes here...");
+}
+
+// Main program
+program.version("1.0.0");
+displayAsciiArt();
+displayMainOptions();
+
+program
+  .command("create")
+  .description("Create a new project")
+  .action(displayCreateOptions);
+
+program
+  .command("test")
+  .description("Run tests")
+  .action(() => {
+    console.log("Running tests...");
+  });
+
+program.parse(process.argv);
--- cli/install.sh
+++ cli/install.sh
@@ -1,44 +1,30 @@
 #!/bin/bash
 
-# Function to install kwami globally
+# Function to install kwami globally with privileges
 function installKwamiGlobally() {
-  echo "Installing kwami-cli globally..."
-  npm install -g ./package/install.js
-
-  # Add the npm global packages path to your system's PATH variable
-  # You might need to adjust this based on your npm global packages path
-  echo "export PATH=\$PATH:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/home/your-username/.npm-global/bin" >> ~/.bashrc
-  source ~/.bashrc
+  echo "Installing kwami globally with privileges..."
+  sudo npm i -g ./package/kwami-cli
 
   # Verify installation
   kwami --version
 }
 
-# Function to install kwami locally
-function installKwamiLocally() {
-  echo "Installing kwami-cli locally..."
-  npm install ./package/install.js
-
-  # Verify installation
-  ./node_modules/.bin/kwami --version
-}
-
 # Check if running with root permissions
 if [[ $EUID -eq 0 ]]; then
-   echo "This script should not be run with 'sudo'. Please run it as a regular user."
-   exit 1
+  echo "This script should not be run with 'sudo'. Please run it as a regular user."
+  exit 1
 fi
 
-# Ask user if they want to install kwami locally or globally
-echo "Do you want to install kwami locally or globally?"
-select installOption in "Locally" "Globally"; do
+# Ask user if they want to install kwami globally with privileges
+echo "Do you want to install kwami globally with privileges?"
+select installOption in "Yes" "No"; do
   case $installOption in
-    Locally)
-      installKwamiLocally
+    Yes)
+      installKwamiGlobally
       break
       ;;
-    Globally)
-      installKwamiGlobally
+    No)
+      echo "Installation canceled."
       break
       ;;
     *)
@@ -47,4 +33,4 @@ select installOption in "Locally" "Globally"; do
   esac
 done
 
-echo "kwami has been installed successfully! You can now use 'kwami' commands."
+echo "Installation completed successfully!"
